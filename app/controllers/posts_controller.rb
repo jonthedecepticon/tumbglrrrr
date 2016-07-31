7df1d60347400975b5f0a1a0ae4e37016eec107d
Jon Lambson <jon@crowdengine.com>
2016-07-30 21:36:26 -0600

    edit and delete post actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,55 @@
 class PostsController < ApplicationController
+  # before_action :find_post, :except[:new, :create, :index]
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
 
-def index
-end
+  def new
+    @post = Post.new
+  end
 
-def new
-end
+  def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to root_path
+  end
+
+  private
+    # def find_post
+    #   @post = Post.find(params[:id])
+    # end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
